5da7393c1d4ffa89f9701ca2fd64d46e0279d63f
Derek Neilson <42622294+dwerkjem@users.noreply.github.com>
2021-04-10 09:27:43 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import torch
+import numpy as np
+import torchvision
+import tkinter as tk
+
+win = tk.Tk()
+
+(program_name) = "Image Recognition Machine Learning Algorithm"
+
+win.title(program_name)
+
+win.mainloop()
