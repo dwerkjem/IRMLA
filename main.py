4016f174a89cadb771875b4cbe6e1bc4050ba5c6
Derek Neilson <42622294+dwerkjem@users.noreply.github.com>
2021-04-12 09:29:29 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -4,14 +4,25 @@ import torchvision
 import sys
 import os
 import tkinter as tk
+from PIL import ImageTk, Image
 from tkinter import *
+from tkinter import filedialog
 
-win = tk.Tk()
 
-win.geometry("500x500")
+def openFile():
+    PNGs = filedialog.askopenfilename()
+    print(PNGs)
+
 
 (program_name) = "Image Recognition Machine Learning Algorithm"
+(defwinsize) = "450x450"
+initaldir = "C:"
+TitleOfOFD = "select PNGs for processing"
+filetypes = "pngs" "*.png"
+win = tk.Tk()
+button = Button(text=TitleOfOFD, command=openFile)
 
 win.title(program_name)
-
+win.geometry(defwinsize)
+button.pack()
 win.mainloop()
