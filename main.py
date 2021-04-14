84b89d0c387b4cf2163047d408b6b8081d97f257
Derek Neilson <42622294+dwerkjem@users.noreply.github.com>
2021-04-14 17:37:05 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import torch
 import numpy as np
-import torchvision
 import sys
+import face_recognition
+import cv2
 import os
 import tkinter as tk
 from PIL import ImageTk, Image
@@ -9,16 +9,22 @@ from tkinter import *
 from tkinter import filedialog
 
 
+# definitions
 def openFile():
-    PNGs = filedialog.askopenfilename()
-    print(PNGs)
+    Tpngs: object = filedialog.askopenfilename()
+    print(Tpngs)
 
+
+# things to do
+# find file in pictures
+# loop over pictures in file name
+# data base SQL lite
+
+img = cv2.imread('Tpngs', 1)
 
 (program_name) = "Image Recognition Machine Learning Algorithm"
 (defwinsize) = "450x450"
-initaldir = "C:"
 TitleOfOFD = "select PNGs for processing"
-filetypes = "pngs" "*.png"
 win = tk.Tk()
 button = Button(text=TitleOfOFD, command=openFile)
 
